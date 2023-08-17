9f35246edb03ad6a21d96964a598d8cc50ab7a26
Dmytro_Romaniv <128440171+dimaromaniv@users.noreply.github.com>
2023-08-17 14:29:35 -0400

    firs commit

---
+++ src/main/java/rocks/zipcode/demo/BeanFlavoredLearnerLabApplication.java
@@ -0,0 +1,13 @@
+package rocks.zipcode.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanFlavoredLearnerLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanFlavoredLearnerLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rocks/zipcode/demo/BeanFlavoredLearnerLabApplicationTests.java
@@ -0,0 +1,13 @@
+package rocks.zipcode.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanFlavoredLearnerLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
